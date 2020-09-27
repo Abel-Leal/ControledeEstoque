6792fb61b55d4de68abd9ed8aaaa255163d401cb
Abel <abelsouzaleal88@outlook.com>
2020-09-27 16:16:04 -0300

    Adicionar arquivos de projeto.

---
+++ ControledeEstoqueTeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ControledeEstoqueTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
